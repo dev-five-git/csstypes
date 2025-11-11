f8a70e3369e0a1f871611d0e2d31fcd3632208d3
owjs3901 <owjs3901@gmail.com>
2025-11-12 07:19:18 +0900

    Implement build

--- build.ts
+++ build.ts
@@ -1,47 +1,97 @@
-import { definitionSyntax } from "css-tree";
 import { convertType } from "./convert-type";
-import { splitType } from "./split-type";
 import { toCamelCase } from "./to-camelcase";
 import { toPascalCase } from "./to-pascalcase";
 
-console.log(definitionSyntax.parse("<bg-layer>#? , <final-bg-layer>"));
-
-let output = `export as namespace CSS;\n`;
+let output = `export as namespace CSS;\nexport interface CustomColors{}\n`;
 
 const syntaxes = (await fetch(
 	"https://raw.githubusercontent.com/mdn/data/refs/heads/main/css/syntaxes.json",
-).then((res) => res.json())) as Record<string, any>;
+).then((res) => res.json())) as Record<
+	string,
+	{
+		syntax: string;
+		primitive?: boolean;
+	}
+>;
+
 const properties = (await fetch(
 	"https://raw.githubusercontent.com/mdn/data/refs/heads/main/css/properties.json",
-).then((res) => res.json())) as Record<string, any>;
+).then((res) => res.json())) as Record<
+	string,
+	{
+		syntax: string;
+		status: string;
+	}
+>;
 
-function getType(property: string, syntax: string) {
-	const sp = splitType(syntax);
-	return (
-		sp
-			.map((type) => convertType(property, type))
-			.filter(Boolean)
-			.join(" | ") || "string"
-	);
-}
+const customSyntaxes = {
+	string: {
+		syntax: "(string & {})",
+		primitive: true,
+	},
+	number: {
+		syntax: "number",
+		primitive: true,
+	},
+	percentage: {
+		syntax: "number",
+		primitive: true,
+	},
+	integer: {
+		syntax: "number",
+		primitive: true,
+	},
+};
+
+Object.assign(syntaxes, customSyntaxes);
 
 const standardProperties: string[] = [];
+const vendorProperties: [string, { syntax: string; status: string }][] = [];
 for (const [property, value] of Object.entries(properties)) {
 	// skip vendor prefixed properties
+	if (property.startsWith("--")) continue;
 	if (
-		property.startsWith("--") ||
 		property.startsWith("-ms-") ||
 		property.startsWith("-webkit-") ||
 		property.startsWith("-moz-")
-	)
+	) {
+		vendorProperties.push([property, value]);
 		continue;
+	}
 	if (value.status !== "standard") continue;
-	output += `export type ${toPascalCase(property)} = ${getType(toPascalCase(property), value.syntax)};\n`;
+	output += `export type ${toPascalCase(property)} = ${convertType(value.syntax, syntaxes)};\n`;
 	standardProperties.push(property);
 }
 
 output += `export interface StandardProperties {
 ${standardProperties.map((p) => `  ${toCamelCase(p)}: ${toPascalCase(p)}`).join("\n")}
-}`;
+}\n`;
+
+for (const [property, value] of vendorProperties) {
+	output += `export type ${toPascalCase(property)} = ${convertType(value.syntax, syntaxes)};\n`;
+}
+
+output += `export interface VendorProperties {
+${vendorProperties.map(([p, _]) => `  ${toCamelCase(p)}: ${toPascalCase(p)}`).join("\n")}
+}\n`;
+
+for (const [property, value] of Object.entries(syntaxes)) {
+	if (
+		property.endsWith(")") ||
+		property.includes("+") ||
+		value.syntax.includes("@")
+	)
+		continue;
+	if (property in customSyntaxes) {
+		continue;
+	}
+	output += `export type T${toPascalCase(property)} = ${convertType(value.syntax.replace("<image |", "<image> |"), syntaxes)}${property === "color" ? " | keyof CustomColors" : ""};\n`;
+}
 
 Bun.write("src/index.d.ts", output);
+
+// declare module "./src" {
+// 	interface CustomColors {
+// 		$example: unknown
+// 	}
+// }
--- convert-type.ts
+++ convert-type.ts
@@ -5,17 +5,21 @@ import { toPascalCase } from "./to-pascalcase";
 /**
  * convert type
  */
-export function convertType(key: string, type: string): string {
+export function convertType(
+	type: string,
+	syntaxs: Record<string, { syntax: string; primitive?: boolean }>,
+): string {
 	const ast = definitionSyntax.parse(type);
-    console.log("ast", JSON.stringify(ast, null, 2));
-	const result = convertNode(key, ast, undefined);
-	return typeof result === "string" ? result : "";
+	return convertNode(ast, syntaxs) || "(string & {})";
 }
 
-function convertNode(key: string, node: DSNode, parent: DSNode | undefined): string {
+function convertNode(
+	node: DSNode,
+	syntaxs: Record<string, { syntax: string; primitive?: boolean }>,
+): string {
 	switch (node.type) {
 		case "Keyword":
-			return node.name;
+			return `"${node.name}"`;
 		case "String":
 			return `"${node.value}"`;
 		case "Type": {
@@ -25,131 +29,29 @@ function convertNode(key: string, node: DSNode, parent: DSNode | undefined): str
 				return toPascalCase(propName);
 			}
 			if (typeName.endsWith("()")) return "";
-			return `T${toPascalCase(typeName)}`;
+			if (syntaxs[typeName]) {
+				if (syntaxs[typeName].primitive) return syntaxs[typeName].syntax;
+				return `T${toPascalCase(typeName)}`;
+			}
+			if (typeName.endsWith("-ident")) return "(string & {})";
+			return "";
 		}
 		case "Multiplier": {
-            if(node.max === 0 && node.min === 0) {
-                return `\`\${${convertNode(key, node.term, node)}} \` | (string & {})`;
-            }
-            
-			if (node.max === 0) {
-				const k = convertNode(key, node.term, node);
-				return wrapLoop(node.term.type === "Keyword" || node.term.type === "Property" ? `"${k}"` : k, key);
-			} else {
-				if (
-					node.min === 0 &&
-					parent?.type === "Group" &&
-					parent.terms.length > 1
-				) {
-					if (parent.terms.indexOf(node) === 0)
-						return `\`\${${convertNode(key, node.term, node)}} \` | ""`;
-					else {
-                        if(node.term.type === "Keyword" || (node.term.type === "Group" && node.term.terms.length > 1 && !node.term.terms.some((term) => term.type === "Group" || term.type === "Multiplier"))) {
-                            return `\` ${convertNode(key, node.term, node)}\` | ""`;
-                        }
-                        return `\` \${${convertNode(key, node.term, node)}}\` | ""`;
-                    }
-				}
-				const contents = [];
-				for (let i = 0; i < node.min; i++) {
-					const k = convertNode(key, node.term, node);
-					contents.push(`\${${k}}`);
-				}
-				for (let i = node.min; i < node.max; i++) {
-					const k = convertNode(key, node.term, node);
-					contents.push(`\${\` \${${k}}\` | ""}`);
-				}
-				return contents.join("");
-			}
+			return convertNode(node.term, syntaxs);
 		}
 		case "Group": {
-			// check multiplier
-			if (
-				node.terms.some((term) => (term.type === "Multiplier" && term.max !== 0) || term.type === "Comma")
-			) {
-				if (node.terms.length === 1) {
-					return `\`${convertNode(key, node.terms[0]!, node)}\``;
-				}
-				const contents = node.terms.filter((term) => term.type !== "Comma")
-					.map((term, idx, array) => {
-                        if(term.type === "Multiplier" && term.min > 0 && term.max > 0 || term.type==="Keyword" || term.type==="Token") {
-                            const spaceAround = term.type === "Token" && idx < array.length - 1 && array[idx + 1]?.type !== "Multiplier"
-                            if(spaceAround) {
-                                return ` ${convertNode(key, term, node)} `
-                            }
-                            const hasPostfix = term.type === "Token" && idx < array.length - 1 && array[idx + 1]?.type === "Multiplier" && array[idx + 1]?.min > 0
-                            if(hasPostfix) {
-                                return `${convertNode(key, term, node)} `
-                            }
-                            return convertNode(key, term, node)
-                        }
-
-                        return `\${${convertNode(key, term, node)}}`
-                    })
-					.filter(Boolean);
-				if (node.disallowEmpty) {
-					const noEmptyContents = node.terms
-						.map((term) => convertNode(key, unwrapMultiplier(term), node))
-						.filter(Boolean);
-					return `${noEmptyContents.join(" | ")} | \`${noEmptyContents.map((content) => `\${${content}}`).join(" ")}\``;
-				} else {
-					return `\`${contents.join("")}\``;
-				}
-			}
-
-			const normalContents = node.terms
-				.map((term) => {
-                    if(term.type === "Keyword" || term.type === "Token") {
-                        return `"${convertNode(key, term, node)}"`
-                    }
-                    return convertNode(key, term, node);
-                })
-				.filter(Boolean);
-			switch (node.combinator) {
-				case "|":
-					return normalContents.join(" | ");
-				case "||":
-					return (
-						normalContents.join(" | ") +
-						" | " +
-						`\`${node.terms
-							.map((node) => {
-								if (node.type === "Keyword")
-									return convertNode(key, node, node);
-								return `\${${convertNode(key, node, node)}}`;
-							})
-
-							.join(" ")}\``
-					);
-				default:
-                    if (node.terms.length > 1) {
-                        const wrap = !!parent || node.terms.some((term) => term.type === "Multiplier" && term.min > 0);
-                        if (wrap) {
-                            return `\`${node.terms.map((node) => {
-                                if(node.type === "Keyword" || node.type === "Token") {
-                                    return convertNode(key, node, node)
-                                }
-                                return `\${${convertNode(key, node, node)}}`;
-                            }).join(" ")}\``;
-                        }
-                        return node.terms.map((node) => {
-                            if(node.type === "Keyword" || node.type === "Token") {
-                                return convertNode(key, node, node)
-                            }
-                            return `\${${convertNode(key, node, node)}}`;
-                        }).join(" ")
-                    }
-					return normalContents.join("");
-			}
+			return removeDuplicates(
+				node.terms.map((term) => convertNode(term, syntaxs)).filter(Boolean),
+			).join(" | ");
 		}
 		case "Function":
 			return "";
 		case "Comma":
-			return " ";
+			return "";
 		case "Property":
 			return toPascalCase(node.name);
 		case "Token":
-			return node.value;
+			return "";
 		case "AtKeyword":
 			return `@${node.name}`;
 		default:
@@ -157,9 +59,7 @@ function convertNode(key: string, node: DSNode, parent: DSNode | undefined): str
 	}
 }
 
-function wrapLoop(first: string, rest: string): string {
-	return `\`\${${first}}\${\` \${${rest}}\` | ""}\``;
-}
-function unwrapMultiplier(node: DSNode) {
-	return node.type === "Multiplier" ? node.term : node;
+// remove duplicates from array
+function removeDuplicates<T>(array: T[]): T[] {
+	return array.filter((value, index, self) => self.indexOf(value) === index);
 }
