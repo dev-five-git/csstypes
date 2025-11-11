e0172d401c4ba51ec01ed3d42d611f6b655f82d1
owjs3901 <owjs3901@gmail.com>
2025-11-12 08:43:32 +0900

    Fix css files

--- build.ts
+++ build.ts
@@ -2,7 +2,7 @@ import { convertType } from './convert-type'
 import { toCamelCase } from './to-camelcase'
 import { toPascalCase } from './to-pascalcase'
 
-let output = `// biome-ignore lint/suspicious/noEmptyInterface: <explanation>\nexport interface CustomColors{}\nexport as namespace CSS;\n`
+let output = `// biome-ignore lint/suspicious/noEmptyInterface: gen\nexport interface CustomColors{}\nexport as namespace CSS;\n`
 
 const syntaxes = (await fetch(
   'https://raw.githubusercontent.com/mdn/data/refs/heads/main/css/syntaxes.json',
@@ -67,11 +67,20 @@ const customSyntaxes = {
 
 Object.assign(syntaxes, customSyntaxes)
 
-const standardPropertyTypes: string[] = []
-const standardLonghandProperties: [string, { syntax: string; status: string }][] = []
-const standardShorthandProperties: [string, { syntax: string; status: string }][] = []
-const vendorLonghandProperties: [string, { syntax: string; status: string }][] = []
-const vendorShorthandProperties: [string, { syntax: string; status: string }][] = []
+const standardLonghandProperties: [
+  string,
+  { syntax: string; status: string },
+][] = []
+const standardShorthandProperties: [
+  string,
+  { syntax: string; status: string },
+][] = []
+const vendorLonghandProperties: [string, { syntax: string; status: string }][] =
+  []
+const vendorShorthandProperties: [
+  string,
+  { syntax: string; status: string },
+][] = []
 for (const [property, value] of Object.entries(properties)) {
   // skip vendor prefixed properties
   if (property.startsWith('--')) continue
@@ -80,17 +89,15 @@ for (const [property, value] of Object.entries(properties)) {
     property.startsWith('-webkit-') ||
     property.startsWith('-moz-')
   ) {
-    if(Array.isArray(value.computed))
+    if (Array.isArray(value.computed))
       vendorShorthandProperties.push([property, value])
-    else
-      vendorLonghandProperties.push([property, value])
+    else vendorLonghandProperties.push([property, value])
     continue
   }
   if (value.status !== 'standard') continue
-  if(Array.isArray(value.computed)) 
+  if (Array.isArray(value.computed))
     standardShorthandProperties.push([property, value])
-  else
-    standardLonghandProperties.push([property, value])
+  else standardLonghandProperties.push([property, value])
 }
 
 output += `export interface StandardLonghandProperties {
@@ -109,16 +116,21 @@ output += `export interface VendorShorthandProperties {
 ${vendorShorthandProperties.map(([p, _]) => `  ${toCamelCase(p).replace(/^Ms/, 'ms')}?: Property.${toPascalCase(p)} | undefined`).join('\n')}
 }\n`
 
-
 // AtRules
-output += `export type AtRules = ${Object.keys(atRules).map((p) => `"${p}"`).join(' | ')}\n`
+output += `export type AtRules = ${Object.keys(atRules)
+  .map((p) => `"${p}"`)
+  .join(' | ')}\n`
 
 // advenced pseudos
-output += `export type AdvancedPseudos = ${Object.keys(selectors).filter((p) => p.includes('(') && p.includes(')')).map((p) => `"${p}"`).join(' | ')}\n`
-output += `export type SimplePseudos = ${Object.keys(selectors).filter((p) => !p.includes('(') && !p.includes(')')).map((p) => `"${p}"`).join(' | ')}\n`
+output += `export type AdvancedPseudos = ${Object.keys(selectors)
+  .filter((p) => p.includes('(') && p.includes(')'))
+  .map((p) => `"${p}"`)
+  .join(' | ')}\n`
+output += `export type SimplePseudos = ${Object.keys(selectors)
+  .filter((p) => !p.includes('(') && !p.includes(')'))
+  .map((p) => `"${p}"`)
+  .join(' | ')}\n`
 output += `export type Pseudos = AdvancedPseudos | SimplePseudos\n`
-
-
 
 output += `export namespace Property {\n`
 for (const [property, value] of standardLonghandProperties) {
@@ -156,4 +168,3 @@ Bun.write('src/index.d.ts', output)
 // 		$example: unknown
 // 	}
 // }
-
